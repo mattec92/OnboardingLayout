ece1a8d624879c949f5880e02a3821fab7e9c49a
Mattias Cederlund <mattec92@hotmail.com>
2016-09-14 21:12:38 +0200

    Merged duplicate switch cases in TextOnboardingElement positionView.

--- lib/src/main/java/se/mattec/onboardinglayout/elements/TextOnboardingElement.java
+++ lib/src/main/java/se/mattec/onboardinglayout/elements/TextOnboardingElement.java
@@ -104,14 +104,12 @@ public class TextOnboardingElement
             case LEFT:
             {
                 params.rightMargin = onboardingLayoutWidth - left;
-//                params.topMargin = top;
                 params.gravity = Gravity.RIGHT;
                 break;
             }
             case RIGHT:
             {
                 params.leftMargin = right;
-//                params.topMargin = top;
                 params.gravity = Gravity.LEFT;
                 break;
             }
@@ -128,25 +126,9 @@ public class TextOnboardingElement
                 int height = view.getHeight();
                 int width = view.getWidth();
 
-                //TODO: Repetetive switch cases
                 switch (location)
                 {
                     case ABOVE:
-                    {
-                        //If there is enough place to center the view
-                        if (viewToAlignCenterHorizontal - width / 2 > 0)
-                        {
-                            if (viewToAlignCenterHorizontal + width / 2 < onboardingLayoutWidth)
-                            {
-                                params.leftMargin = viewToAlignCenterHorizontal - width / 2;
-                            }
-                            else
-                            {
-                                params.leftMargin = onboardingLayoutWidth - width;
-                            }
-                        }
-                        break;
-                    }
                     case BELOW:
                     {
                         //If there is enough place to center the view
@@ -164,28 +146,6 @@ public class TextOnboardingElement
                         break;
                     }
                     case LEFT:
-                    {
-                        //If the view is smaller than the view to align, center it inside the view
-                        if (height < viewToAlignHeight)
-                        {
-                            params.topMargin = top + (viewToAlignHeight - height) / 2;
-                        }
-                        //Otherwise center it outside the view.
-                        else
-                        {
-                            int topMarginCenteredOutside = top - (height - viewToAlignHeight) / 2;
-                            int bottomMostPosition = onboardingLayoutHeight - height;
-                            if (topMarginCenteredOutside < bottomMostPosition)
-                            {
-                                params.topMargin = Math.max(0, topMarginCenteredOutside);
-                            }
-                            else
-                            {
-                                params.topMargin = bottomMostPosition;
-                            }
-                        }
-                        break;
-                    }
                     case RIGHT:
                     {
                         //If the view is smaller than the view to align, center it inside the view
